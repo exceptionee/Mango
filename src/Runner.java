96c850695dee6421d1a9ab92512833923d4ee355
eliasciur <afakedemail@yahoo.com>
2024-06-03 17:01:56 -0500

    cleaned up code

--- src/Runner.java
+++ src/Runner.java
@@ -1,10 +1,9 @@
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Arrays;
-import org.antlr.v4.gui.TreeViewer;
+import java.io.InputStreamReader;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.IntStream;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -12,13 +11,24 @@ import picocli.CommandLine.Parameters;
 
 @Command(name = "mango", description = "The mango interpreter.", mixinStandardHelpOptions = true, version = "mango v0.1.0a")
 public class Runner implements Runnable {
-  @Parameters(index = "0..*", description = "The files to interpret.", arity = "1+")
-  private static File[] files = {};
-
+  @Parameters(index = "0..*", description = "The files to interpret.", arity = "0..*")
+  private static String[] files = {};
   @Option(names = "--gui", description = "Determines whether mango will show a GUI of the parse tree.", defaultValue = "false")
   private static boolean gui;
 
-  private static File file;
+  protected static String file = "REPL";
+
+  private static MangoLexer lexer = new MangoLexer(null);
+  private static MangoParser parser = new MangoParser(null);
+  private static TypeChecker typeChecker = new TypeChecker();
+
+  static {
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(new ErrorListener());
+
+    parser.removeErrorListeners();
+    parser.addErrorListener(new ErrorListener());
+  }
 
   public static void main(String[] args) throws Exception {
     new CommandLine(new Runner()).execute(args);
@@ -26,68 +36,57 @@ public class Runner implements Runnable {
 
   @Override
   public void run() {
-    for (File file : files) {
-      if (!file.getName().endsWith(".mango")) {
-        System.out.println("error: file does not end in '.mango'");
-        continue;
-      }
+    if (files.length > 0) {
+      for (String file : files) {
+        if (!file.endsWith(".mango")) {
+          System.out.println("error: file does not end in '.mango'");
+          continue;
+        }
 
+        try {
+          Runner.file = file;
+          interpret(CharStreams.fromFileName(file));
+        }
+        catch (IOException e) {
+          System.out.println("error: file not found " + file);
+        }
+      }
+    }
+    else {
       try {
-        interpret(file);
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+          System.out.print("> ");
+          String input = r.readLine();
+    
+          if (input == null)
+            break;
+          else if (input.isEmpty())
+            continue;
+    
+          lexer.setInputStream(CharStreams.fromString(input));
+          parser.setTokenStream(new CommonTokenStream(lexer));
+          Type result = typeChecker.visitProgram(parser.program());
+    
+          if (!Error.error)
+            System.out.println("\u001b[38;2;0;140;40m" + result + "\u001B[0m");
+          else {
+            Error.error = false;
+            typeChecker.stack.getFirst().entrySet()
+              .removeIf(e -> e.getValue().type() == null);
+          }
+        }
       }
       catch (IOException e) {
-        System.out.println("error: file not found " + file.getPath());
+        e.printStackTrace();
       }
     }
   }
 
-  public static void interpret(File file) throws IOException {
-    Runner.file = file;
-
-    // tokenizes the file
-    MangoLexer lexer = new MangoLexer(CharStreams.fromFileName(file.getAbsolutePath()));
-    lexer.removeErrorListeners();
-    lexer.addErrorListener(new ErrorListener());
-
-    // parses the tokens from the lexer
-    MangoParser parser = new MangoParser(new CommonTokenStream(lexer));
-    parser.removeErrorListeners();
-    parser.addErrorListener(new ErrorListener());
-
-    // creates the parse tree
-    ParseTree tree = parser.program();
-
-    TypeChecker typeChecker = new TypeChecker();
-    // performs type checking
-    // long start = System.nanoTime();
-    typeChecker.visit(tree);
-    // long stop = System.nanoTime();
-    // System.out.println(stop - start);
-
-    if (gui) {
-      new TreeViewer(Arrays.asList(parser.getRuleNames()), tree).open();
-    }
-
-    if (Error.error) return;
-
-    Interpreter interpreter = new Interpreter();
-    // // interprets the tree
-    try {
-      long start = System.nanoTime();
-      interpreter.visit(tree);
-      long stop = System.nanoTime();
-      System.out.println(stop - start);
-    } catch (OutOfMemoryError e) {
-      new RuntimeError("out of memory", new Source(getFileName(), 0, 0));
-    }
-
-    // opens a window with the parse tree if the --gui flag is set
-    if (gui) {
-      new TreeViewer(Arrays.asList(parser.getRuleNames()), tree).open();
-    }
-  }
-
-  public static String getFileName() {
-    return Runner.file.getName();
+  private static void interpret(IntStream stream) {
+    lexer.setInputStream(stream);
+    parser.setTokenStream(new CommonTokenStream(lexer));
+    typeChecker.visit(parser.program());
   }
 }
