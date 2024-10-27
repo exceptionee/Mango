16e947aca936284194dca3c8b83b242a1a5fc144
eliasciur <afakedemail@yahoo.com>
2024-10-27 12:22:02 -0500

    added CLI library and fixed imports

--- main.cpp
+++ main.cpp
@@ -1,46 +1,85 @@
-#include <iostream>
+#include <any>
 #include <fstream>
-#include <memory>
+#include <iostream>
+#include <string>
+#include "argparse.hpp"
+#include "ASTNode.h"
+#include "Error.h"
+#include "Interpreter.h"
 #include "Lexer.h"
 #include "Parser.h"
-#include "ASTNode.h"
-#include "Visitor.h"
-#include "Interpreter.h"
-#include "Value.cpp"
-using namespace std;
+#include "Type.h"
+#include "TypeChecker.h"
+#include "Value.h"
 
-Value interpret(string input) {
+std::string Source::file = "REPL";
+
+Value interpret(std::string input) {
   Program* AST = Parser.parse(Lexer.tokenize(input));
+  TypeChecker.visitProgram(*AST);
 
-  if (!hadError) return any_cast<Value>(Interpreter.visitProgram(*AST));
+  if (!hadError) return std::any_cast<Value>(Interpreter.visitProgram(*AST));
   return {};
 }
 
 int main(int argc, char* argv[]) {
-  std::ios::sync_with_stdio(false);
-  cout << showpoint;
+  argparse::ArgumentParser program("mango", "v0.1.0a");
 
-  if (argc == 1) {
-    string input;
+  program.add_argument("file")
+    .default_value("")
+    .help("the file to interpret");
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::runtime_error& err) {
+    std::cerr << "error: " << err.what() << "\n";
+    return 1;
+  }
+
+  std::ios::sync_with_stdio(false);
+  std::cout << std::showpoint;
+
+  std::string fileName = program.get<std::string>("file");
+
+  if (fileName.empty()) {
+    std::string input;
 
     while (true) {
-      cout << "> ";
-      std::getline(cin, input);
+      std::cout << "> ";
+      std::getline(std::cin, input);
 
-      if (cin.eof()) break;
+      if (std::cin.eof()) break;
       else if (input.empty()) continue;
 
       Value result = interpret(input);
 
-      if (!hadError) cout << result << endl;
-      else hadError = false;
+      if (!hadError) std::cout << result << std::endl;
+      else {
+        hadError = false;
+
+        for (auto it = TypeChecker.stack.back().begin(); it != TypeChecker.stack.back().end();) {
+          if (it->second.type == ERROR_T)
+            it = TypeChecker.stack.back().erase(it);
+          else ++it;
+        }
+      }
     }
   }
   else {
-    for (int i = 1; i < argc; ++i) {
-      ifstream file { argv[i] };
-      string str { istreambuf_iterator<char>(file), istreambuf_iterator<char>() };
-      interpret(str);
+    if (fileName.size() <= 6 || fileName.substr(fileName.size() - 6) != ".mango") {
+      std::cerr << "error: file does not end in '.mango'";
+      return 1;
     }
+
+    std::ifstream file(fileName);
+
+    if (!file.good()) {
+      std::cerr << "error: file does not exist";
+      return 1;
+    }
+
+    Source::file = fileName;
+    std::string str{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+    interpret(str);
   }
 }
